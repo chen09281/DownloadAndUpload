0593bfb088c8ab47cbf5417d71306b2a77c85796
Chen <weifengchen198@gmial.com>
2022-12-03 14:47:31 +0800

    修复新增用户的bug

--- src/main/java/com/chen/mapper/UserMapper.java
+++ src/main/java/com/chen/mapper/UserMapper.java
@@ -12,6 +12,6 @@ public interface UserMapper {
     @Select("select * from user")
     User selectAll();
 
-    @Insert("insert into user (user_id,user_name,user_password,user_phone,user_email) values (null,#{user.userName},#{user.userPassword},#{user.Phone},#{user.Email})")
+    @Insert("insert into user (user_id,user_name,user_password,user_phone,user_email) values (null,#{user.userName},#{user.userPassword},#{user.userPhone},#{user.userEmail})")
     void addUser(@Param("user") User user);
 }
